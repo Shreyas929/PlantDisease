a286ae60efb638fd793221300261065121428119
Shreyas Shetty <shreyasshetty929@gmail.com>
2025-06-17 11:09:40 +0530

    Update app.py
    remove commit

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 print("hello")
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 print("this is model")
 print("this is shreyas")
-=======
 print("this is model")
->>>>>>> Stashed changes
-=======
 print("this is model")
 print("my name is deekshith")
 print("hello")
