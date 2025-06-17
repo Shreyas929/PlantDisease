98738122d27254f29174999c9baa46aaa56bc729
Shreyas Shetty <shreyasshetty929@gmail.com>
2025-06-17 11:06:46 +0530

    Update app.py
    changes tfyfyf

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 print("hello")
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("this is model")
 print("this is shreyas")
+=======
+print("this is model")
+>>>>>>> Stashed changes
 =======
 print("this is model")
 >>>>>>> Stashed changes
